fefa2e5ec9c95500ae992c0cd8cd348c3d4da802
sfm <2231962562@qq.com>
2020-01-21 12:50:42 +0800

    用element-ui实现的表单组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,82 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <template>
+      <!-- {{value}} -->
+      <K-form :model="model" :rules="rules">
+        <K-form-Item label="用户名" prop="username">
+          <K-input v-model="model.username"></K-input>
+        </K-form-Item>
+        <K-form-Item label="密码" prop="password">
+          <K-input v-model="model.password" type=""></K-input>
+        </K-form-Item>
+      </K-form>
+      <div>
+        <h3>Element表单</h3>
+        <hr>
+        <el-form :model="model" :rules="rules" ref="loginForm">
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="model.username" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="password">
+            <el-input type="password" v-model="model.password" autocomplate="off">
+            </el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </template>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+import KInput from './components/input'
+import KFormItem from './components/FormItem'
+import KForm from './components/Form'
+export default {
+  name: 'app',
+  components: {
+    KInput,
+    KFormItem,
+    KForm
+  },
+  data(){
+    return{
+      value:'',
+      model: {username: "tom",password: ""},
+      rules: {
+        username: [
+          {required: true, message: "请输入用户名"},
+          {min:6,max:10, message: "请输入6~10的用户名"},
+        ],
+        password: [
+          {required: true, message: "请输入密码"}
+        ],
+      }
+    };
+  },
+  methods: {
+    submitForm(form) {
+      this.$refs[form].validate(valid=>{
+        if(valid){
+          alert('请求登录！')
+        }else{
+          alert('效验失败！')
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
---
+++ src/components/FormItem.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <label for="">{{label}}</label>
+        <div>
+            <slot></slot>
+            <p v-if="errStatus">{{errMessage}}</p>
+        </div>
+    </div>
+</template>
+<script>
+    import Schema from 'async-validator'
+    export default{
+        // inject: ['Kform'],
+        props:['label','prop'],
+        data(){
+            return{
+                errMessage: '',
+                errStatus: false
+            }
+        },
+        mounted(){
+            this.$on('validate',this.validator)
+        },
+        methods:{
+            validator(){
+                //有两个Input，一个用户名，一个密码
+                const rules = this.KForm.rules[this.prop];
+                const value = this.KForm.model[this.prop];
+
+                const descriptor = {[this.prop]:rules};
+                // console.log('开始校验了')
+                const schema = new Schema({descriptor});
+                schema.validate({[this.prop]:value},errors =>{
+                    if(errors){
+                        this.errMessage = errors[0].message;
+                        this.errStatus = true;
+                    }else{
+                        this.errMessage = '';
+                        this.errStatus = '';
+                    }
+                })
+            }
+        }
+    }
+</script>
+<style scoped>
+
+</style>
